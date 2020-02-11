aecd8cdd41d6f5a5231c921fe34049147a5570ef
rsmilek <smilek.radim@gmail.com>
2020-02-11 06:28:25 +0100

    r-counter: + added navigarion bar component App component modified to consists of two components NavBar and Counters

---
+++ r-counter-app/src/componets/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        {/* <a className="navbar-brand" href="#">
+          Navbar
+        </a> */}
+        <span className="navbar-brand mb-0 h1">Navbar</span>
+      </nav>
+    );
+  }
+}
