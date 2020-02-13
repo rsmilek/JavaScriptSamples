0940b4beddd9505e906f8fbd48670006cb8675e9
rsmilek <smilek.radim@gmail.com>
2020-02-13 21:21:36 +0100

    r-chartjs: +added init version

---
+++ r-chartjs/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LineGraph from "./components/lineGraph";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LineGraph />
+    </div>
+  );
+}
+
+export default App;
---
+++ r-chartjs/src/components/lineGraph.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Chart from "chart.js";
+//import classes from "./LineGraph.module.css";
+
+export default class LineGraph extends Component {
+  chartRef = React.createRef();
+
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+
+    new Chart(myChartRef, {
+      type: "line",
+      data: {
+        //Bring in data
+        labels: ["Jan", "Feb", "March"],
+        datasets: [
+          {
+            label: "Sales",
+            data: [86, 67, 91]
+          }
+        ]
+      },
+      options: {
+        //Customize chart options
+      }
+    });
+  }
+  render() {
+    return (
+      // <div className={classes.graphContainer}>
+      <div>
+        <canvas id="myChart" ref={this.chartRef} />
+      </div>
+    );
+  }
+}
