27b8c90ab13194046c182c62d70309b91613ed38
rsmilek <smilek.radim@gmail.com>
2020-02-14 10:53:38 +0100

    r-iot-chart: * Chart shows day temperature data

--- r-iot-chart/src/components/chart.jsx
+++ r-iot-chart/src/components/chart.jsx
@@ -2,16 +2,116 @@ import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
 
 const state = {
-  labels: ["January", "February", "March", "April", "May"],
   datasets: [
     {
-      label: "Rainfall",
+      label: "Temp",
       fill: false,
       lineTension: 0.5,
       backgroundColor: "rgba(75,192,192,1)",
       borderColor: "rgba(0,0,0,1)",
       borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
+      data: [
+        {
+          t: "2020-02-12T00:00",
+          y: 5
+        },
+        {
+          t: "2020-02-12T01:00",
+          y: 5
+        },
+        {
+          t: "2020-02-12T02:00",
+          y: 5
+        },
+        {
+          t: "2020-02-12T03:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T04:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T05:00",
+          y: 1
+        },
+        {
+          t: "2020-02-12T06:00",
+          y: 1
+        },
+        {
+          t: "2020-02-12T07:00",
+          y: 1
+        },
+        {
+          t: "2020-02-12T08:00",
+          y: 1
+        },
+        {
+          t: "2020-02-12T09:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T10:00",
+          y: 4
+        },
+        {
+          t: "2020-02-12T11:00",
+          y: 5
+        },
+        {
+          t: "2020-02-12T12:00",
+          y: 6
+        },
+        {
+          t: "2020-02-12T13:00",
+          y: 6
+        },
+        {
+          t: "2020-02-12T14:00",
+          y: 7
+        },
+        {
+          t: "2020-02-12T15:00",
+          y: 7
+        },
+        {
+          t: "2020-02-12T16:00",
+          y: 6
+        },
+        {
+          t: "2020-02-12T17:00",
+          y: 4
+        },
+        {
+          t: "2020-02-12T18:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T19:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T20:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T21:00",
+          y: 3
+        },
+        {
+          t: "2020-02-12T22:00",
+          y: 2
+        },
+        {
+          t: "2020-02-12T23:00",
+          y: 2
+        },
+        {
+          t: "2020-02-12T24:00",
+          y: 2
+        }
+      ]
     }
   ]
 };
@@ -23,9 +123,34 @@ export default class Chart extends Component {
         <Line
           data={state}
           options={{
+            scales: {
+              xAxes: [
+                {
+                  type: "time",
+                  time: {
+                    unit: "hour",
+                    stepSize: 3,
+                    displayFormats: {
+                      hour: "HH:mm"
+                    }
+                  }
+                  //   ticks: {
+                  //     min: "2020-02-12T00:00",
+                  //     max: "2020-02-13T00:00"
+                  //   }
+                }
+              ],
+              yAxes: [
+                {
+                  ticks: {
+                    stepSize: 2
+                  }
+                }
+              ]
+            },
             title: {
               display: true,
-              text: "Average Rainfall per month",
+              text: "Temperature per hour",
               fontSize: 20
             },
             legend: {
