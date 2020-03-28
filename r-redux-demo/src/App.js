1e751177f5b2f710a39fe8cb00a5f9cc9a6a5c90
rsmilek <smilek.radim@gmail.com>
2020-03-28 18:43:26 +0100

    r-redux-demo: + added redux store & providing in App

--- r-redux-demo/src/App.js
+++ r-redux-demo/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import "./components/cakeContainer";
 import CakeContainer from "./components/cakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    // Providing Redux store to application
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
