93603a13a7e7e31ad5212a1eda916fd7e74af2e3
rsmilek <smilek.radim@gmail.com>
2020-03-29 11:17:31 +0200

    r-iot-chart: * refactoring of data series to separate files

--- r-iot-chart/src/components/iotChart.jsx
+++ r-iot-chart/src/components/iotChart.jsx
@@ -1,145 +1,13 @@
 import React, { Component } from "react";
 import Chart from "chart.js";
+import { SERIES_DAY } from "../data/seriesDay";
+import { SERIES_WEEK } from "../data/seriesWeek";
 
 export default class IotChart extends Component {
-  series = [
-    {
-      t: "2020-02-12T00:00",
-      y: 5
-    },
-    {
-      t: "2020-02-12T01:00",
-      y: 5
-    },
-    {
-      t: "2020-02-12T02:00",
-      y: 5
-    },
-    {
-      t: "2020-02-12T03:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T04:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T05:00",
-      y: 1
-    },
-    {
-      t: "2020-02-12T06:00",
-      y: 1
-    },
-    {
-      t: "2020-02-12T07:00",
-      y: 1
-    },
-    {
-      t: "2020-02-12T08:00",
-      y: 1
-    },
-    {
-      t: "2020-02-12T09:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T10:00",
-      y: 4
-    },
-    {
-      t: "2020-02-12T11:00",
-      y: 5
-    },
-    {
-      t: "2020-02-12T12:00",
-      y: 6
-    },
-    {
-      t: "2020-02-12T13:00",
-      y: 6
-    },
-    {
-      t: "2020-02-12T14:00",
-      y: 7
-    },
-    {
-      t: "2020-02-12T15:00",
-      y: 7
-    },
-    {
-      t: "2020-02-12T16:00",
-      y: 6
-    },
-    {
-      t: "2020-02-12T17:00",
-      y: 4
-    },
-    {
-      t: "2020-02-12T18:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T19:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T20:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T21:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T22:00",
-      y: 2
-    },
-    {
-      t: "2020-02-12T23:00",
-      y: 2
-    },
-    {
-      t: "2020-02-12T24:00",
-      y: 2
-    }
-  ];
-
-  series2 = [
-    {
-      t: "2020-02-12T00:00",
-      y: 1
-    },
-    {
-      t: "2020-02-12T01:00",
-      y: 2
-    },
-    {
-      t: "2020-02-12T02:00",
-      y: 3
-    },
-    {
-      t: "2020-02-12T03:00",
-      y: 4
-    },
-    {
-      t: "2020-02-12T04:00",
-      y: 5
-    },
-    {
-      t: "2020-02-12T05:00",
-      y: 6
-    },
-    {
-      t: "2020-02-12T06:00",
-      y: 7
-    }
-  ];
-
   data = {
     datasets: [
       {
-        label: "Temp",
+        label: "Outdoor",
         fill: false,
         lineTension: 0,
         backgroundColor: "rgba(75,192,192,1)",
@@ -219,9 +87,9 @@ export default class IotChart extends Component {
   // Assign chart's data depending on flag
   resolveChartData() {
     if (this.state.flag) {
-      this.data.datasets[0].data = this.series;
+      this.data.datasets[0].data = SERIES_DAY;
     } else {
-      this.data.datasets[0].data = this.series2;
+      this.data.datasets[0].data = SERIES_WEEK;
     }
   }
 
