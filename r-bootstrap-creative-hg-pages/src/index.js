0fe0be0523aaf99f56a820257b85a87638fca2ac
rsmilek <smilek.radim@gmail.com>
2020-06-17 14:48:48 +0200

    r-bootstrap-creative-gh-pages: * js scripts (jquery, jquery.easing, bootstrap, magnific.popup) replaced from CDN to webpack bundle.

--- r-bootstrap-creative-hg-pages/src/index.js
+++ r-bootstrap-creative-hg-pages/src/index.js
@@ -1,7 +1,8 @@
+import "magnific-popup/dist/magnific-popup.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 // import './index.css';
