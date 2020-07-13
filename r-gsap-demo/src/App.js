3f47b6f40a87c91f98e419db2ae90a5598fc6c8f
rsmilek <smilek.radim@gmail.com>
2020-07-13 15:35:57 +0200

    r-gsap-demo: +added basic routing

--- r-gsap-demo/src/App.js
+++ r-gsap-demo/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import GsapTweenLine from "./GsapTweenLine";
+import GsapTweenLine2 from "./GsapTweenLine2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* Set up the Links */}
+        <div className="navigation">
+          <div className="navigation-sub">
+            <Link to="/" className="navigation-item">
+              Home
+            </Link>
+            <Link to="/tweenLine" className="navigation-item">
+              TweenLine
+            </Link>
+            <Link to="/tweenLine2" className="navigation-item">
+              TweenLine2
+            </Link>
+          </div>
+        </div>
+        {/* Set up the Router */}
+        <Route exact path="/" component={Home} />
+        <Route path="/tweenLine" component={GsapTweenLine} />
+        <Route path="/tweenLine2" component={GsapTweenLine2} />
+      </div>
+    </BrowserRouter>
   );
 }
 
