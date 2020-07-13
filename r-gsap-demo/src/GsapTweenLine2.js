1568b8c8a99b1377d95d25b79c698a17dbaddfb3
rsmilek <smilek.radim@gmail.com>
2020-07-13 15:37:39 +0200

    r-gsap-demo: +added TweenLiteLite example

---
+++ r-gsap-demo/src/GsapTweenLine2.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { TimelineLite } from "gsap/all";
+import "./App.css";
+
+/*
+ The main benefit of storing a TweenLite tween as a reference in the component, is that this pattern allows us to use 
+ any of the methods GSAP has to offer like: play(), pause(), reverse(), restart(), seek(), change the speed (timeScale), etc., 
+ to get full control of the animations. Also this approach allows us to create any GSAP animation (TweenLite, TweenMax, TimelineLite, etc.) 
+ in the constructor. For example, we could use a timeline in order to create a complex animation:
+*/
+
+export default class GsapTweenLine2 extends Component {
+  constructor(props) {
+    super(props);
+    this.myElement = null;
+    this.myTween = new TimelineLite({ paused: true });
+  }
+
+  componentDidMount() {
+    this.myTween
+      .to(this.myElement, 0.5, { x: 100 })
+      .to(this.myElement, 0.5, { y: 100, rotation: 180 })
+      .play();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div ref={(id) => (this.myElement = id)}>GsapTweenLine</div>{" "}
+      </div>
+    );
+  }
+}
