4705e8018c0ebab441aa64e3580152ab4564c3a2
rsmilek <smilek.radim@gmail.com>
2020-07-21 22:29:33 +0200

    r-gsap-demo: *refactoring to component TitleIcon

--- r-gsap-demo/src/GsapTitleIcon.js
+++ r-gsap-demo/src/GsapTitleIcon.js
@@ -1,19 +1,21 @@
+// @flow
 import React, { Component } from "react";
 import gsap from "gsap";
 import { ReactComponent as Logo } from "./logo.svg";
 import "./GsapTitleIcon.css";
 
-const TitleIcon = ({ icons2, iconElements2, titleElements2 }) => {
+const TitleIcon = ({ items }) => {
+  console.log(items);
   return (
     <div className="portfolio-container">
-      {icons2.map((icon, index) => (
+      {items.map((item, index) => (
         <div key={index} className="portfolio-wrapper">
           <div className="portfolio-box">
-            <div className="img-fluid" ref={iconElements2[index]}>
-              {icon}
+            <div className="img-fluid" ref={item.iconElementRef}>
+              {item.icon}
             </div>
-            <div className="portfolio-box-caption" ref={titleElements2[index]}>
-              <div className="project-name">Angular</div>
+            <div className="portfolio-box-caption" ref={item.titleElementRef}>
+              <div className="project-name">{item.title}</div>
             </div>
           </div>
         </div>
@@ -33,112 +35,37 @@ export default class GsapTitleIcon extends Component {
       this.iconElements[i] = el;
     };
 
-    this.icons2 = [<Logo />, <Logo />, <Logo />, <Logo />, <Logo />, <Logo />];
-    this.iconElements2 = [];
-    this.titleElements2 = [];
-    for (let index = 0; index < this.icons2.length; index++) {
-      this.iconElements2[index] = React.createRef();
-      this.titleElements2[index] = React.createRef();
-    }
+    this.icons2 = [
+      { icon: <Logo />, title: "React", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+      { icon: <Logo />, title: "Angular", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+      { icon: <Logo />, title: ".NET", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+      { icon: <Logo />, title: "Visual Studio", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+      { icon: <Logo />, title: "National Instruments", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+      { icon: <Logo />, title: "Java", iconElementRef: React.createRef(), titleElementRef: React.createRef() },
+    ];
   }
 
   componentDidMount() {
     const STAGGER = 0.3;
     const DURATION = 6 * STAGGER;
 
-    // console.log(this.titleElements[0]);
-    // console.log(this.titleElements2[0].current);
-
-    const titleElementsCurrent = this.titleElements2.map(title => title.current);
-    const iconElementsCurrent = this.iconElements2.map(icon => icon.current);
+    const iconElements2Current = this.icons2.map((item) => item.iconElementRef.current);
+    const titleElements2Current = this.icons2.map((item) => item.titleElementRef.current);
 
     this.myTween
       // Title - Show
-      .to(
-        this.titleElements,
-        {
-          duration: DURATION,
-          scale: 1,
-          opacity: 1,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        "Start"
-      )
+      .to(this.titleElements, { duration: DURATION, scale: 1, opacity: 1, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, "Start")
       // Title - Hide
-      .to(
-        this.titleElements,
-        {
-          duration: DURATION,
-          scale: 1,
-          opacity: 0,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        STAGGER + 0.1
-      )
+      .to(this.titleElements, { duration: DURATION, scale: 1, opacity: 0, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, STAGGER + 0.1)
       // Icon - Show
-      .from(
-        this.iconElements,
-        {
-          duration: DURATION,
-          scale: 0.5,
-          opacity: 0,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        2 * STAGGER
-      )
+      .from(this.iconElements, { duration: DURATION, scale: 0.5, opacity: 0, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, 2 * STAGGER)
       //
-      //
-      // Title - Show
-      .to(
-        titleElementsCurrent,
-        {
-          duration: DURATION,
-          scale: 1,
-          opacity: 1,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        "Start"
-      )
-      // Title - Hide
-      .to(
-        titleElementsCurrent,
-        {
-          duration: DURATION,
-          scale: 1,
-          opacity: 0,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        STAGGER + 0.1
-      )
-      // Icon - Show
-      .from(
-        iconElementsCurrent,
-        {
-          duration: DURATION,
-          scale: 0.5,
-          opacity: 0,
-          delay: 0,
-          stagger: STAGGER,
-          ease: "elastic",
-          force3D: true,
-        },
-        2 * STAGGER
-      )
+      // Title2 - Show
+      .to(titleElements2Current, { duration: DURATION, scale: 1, opacity: 1, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, "Start")
+      // Title2 - Hide
+      .to(titleElements2Current, { duration: DURATION, scale: 1, opacity: 0, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, STAGGER + 0.1)
+      // Icon2 - Show
+      .from(iconElements2Current, { duration: DURATION, scale: 0.5, opacity: 0, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true }, 2 * STAGGER)
       .play();
   }
 
@@ -214,11 +141,7 @@ export default class GsapTitleIcon extends Component {
           </div>
         </div>
 
-        <TitleIcon
-          icons2={this.icons2}
-          iconElements2={this.iconElements2}
-          titleElements2={this.titleElements2}
-        />
+        <TitleIcon items={this.icons2} />
       </React.Fragment>
     );
   }
