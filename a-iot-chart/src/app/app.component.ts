014924d0bb5c5c9dcf0ff348bfe106bf689ecca6
Radim Smilek <smilek.radim@gmail.com>
2021-04-03 16:57:08 +0200

    a-iot-chart: + added interval component concept

--- a-iot-chart/src/app/app.component.ts
+++ a-iot-chart/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+const INTERVAL_DEFAULT = 'Day';
+const INTERVALS: string[] = [INTERVAL_DEFAULT, 'Month', 'Year'];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'a-iot-chart';
+
+  intervals: string[] = INTERVALS;
+
+  activeIdx: number = INTERVALS.indexOf(INTERVAL_DEFAULT);
+
+  itemChanged(index: number) {
+    console.log(`Item ${index} clicked!`);
+    this.activeIdx = index;
+  }
 }
