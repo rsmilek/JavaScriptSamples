18d3a8435684a236af83e7d9a644ad652648fb3a
Radim Smilek <smilek.radim@gmail.com>
2021-04-03 19:26:26 +0200

    a-chartjs: + added Angular chartJS sample.

--- a-iot-chart/src/app/app.component.ts
+++ a-iot-chart/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
 const INTERVAL_DEFAULT = 'Day';
 const INTERVALS: string[] = [INTERVAL_DEFAULT, 'Month', 'Year'];
 
@@ -19,4 +22,24 @@ export class AppComponent {
     console.log(`Item ${index} clicked!`);
     this.activeIdx = index;
   }
+
+
+  public lineChartData: ChartDataSets[] = [ { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' } ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+  public lineChartPlugins = [];
+
+  constructor() { }
+
+  ngOnInit() { }
 }
