11725060b1dc20a59b8d8b593ea3f94c827a906e
Radim Smilek <smilek.radim@gmail.com>
2021-04-06 21:54:00 +0200

    + added chart data by interval

--- jq-iot-chart/js/script.js
+++ jq-iot-chart/js/script.js
@@ -5,25 +5,17 @@ const INTERVAL_DEFAULT_CHILD = INTERVAL_DEFAULT_INDEX + 1;
 Chart.defaults.global.legend.display = true;
 Chart.defaults.global.tooltips.enabled = true;
 
-var data = {
+const CHART_DATA_DEFAULT = {
     labels: ["Brasil", "Argentina", "Chile", "México", "Peru", "Paraguai", "Bolívia", "Haiti"],
     datasets: [{
-        data: [31.60, 36.57, 23.49, 23.52, 20.34, 21.99, 35.31, 19.37],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)']
-    }]
-};
-
-var data2 = {
-    labels: ["Brasil", "Argentina", "Chile", "México", "Peru", "Paraguai", "Bolívia", "Haiti"],
-    datasets: [{
-        data: [41.90, 43.95, 25.83, 27.59, 22.58, 24.45, 41.94, 21.96],
+        data: [],
         backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)']
     }]
 };
 
 var myChart = new Chart($("#myChart"), {
     type: 'polarArea',
-    data: data,
+    data: CHART_DATA_DEFAULT,
     options: {
         responsive: true,
         scale: {
@@ -41,27 +33,20 @@ $("#intevals li a").on("click", function (e) {
     e.preventDefault();
 
     $("#intevals").children("li.page-item").removeClass("active");
-
     $(this).parent("li.page-item").addClass("active");
 
     var idx = INTERVALS.indexOf($(this).text());
-
     switch (idx) {
-        case 1:
-            myChart.data = data2;
+        case 0:
+            myChart.data.datasets[0].data = [31.60, 36.57, 23.49, 23.52, 20.34, 21.99, 35.31, 19.37];
             break;
-        default:
-            myChart.data = data;
+        case 1:
+            myChart.data.datasets[0].data = [41.90, 43.95, 25.83, 27.59, 22.58, 24.45, 41.94, 21.96];
+            break;
+        case 2:
+            myChart.data.datasets[0].data = [46.90, 44.95, 26.83, 29.59, 24.58, 26.45, 46.94, 22.96];
             break;
     }
-
-    // console.log($(this));
-    // var foo = eval($(this).data('chart'));
-    // console.log(foo);
-
-    // myChart.data.datasets[0] = data2;
-    // myChart.data = data2;
-    // myChart.data.labels = foo['labels'];
 
     myChart.update();
 });
