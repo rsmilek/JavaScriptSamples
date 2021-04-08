d25c4680bcc6c9b41ab2fa38c7804e3c8be26160
Radim Smilek <smilek.radim@gmail.com>
2021-04-08 15:13:59 +0200

    a-iot-chart: +improved angular class definition (ngOnInit)

--- a-iot-chart/src/app/app.component.ts
+++ a-iot-chart/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Chart } from 'chart.js';
 
@@ -14,7 +14,7 @@ const INTERVALS: string[] = [INTERVAL_DEFAULT, 'Month', 'Year'];
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a-iot-chart';
 
   private data = {
