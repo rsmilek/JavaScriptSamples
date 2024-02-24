a1ba3b579ef73228a422965d84985bcfd53a1b27
rsmilek <rsmilek@seznam.cz>
2024-02-24 17:37:58 +0100

    + added FE part of app

---
+++ abp_todolist.bak/aspnet-core/abp_todolist/Program.cs
@@ -0,0 +1,76 @@
+using abp_todolist.Data;
+using Serilog;
+using Serilog.Events;
+using Volo.Abp.Data;
+
+namespace abp_todolist;
+
+public class Program
+{
+    public async static Task<int> Main(string[] args)
+    {
+        var loggerConfiguration = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(c => c.File("Logs/logs.txt"))
+            .WriteTo.Async(c => c.Console());
+
+        if (IsMigrateDatabase(args))
+        {
+            loggerConfiguration.MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning);
+            loggerConfiguration.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+        }
+
+        Log.Logger = loggerConfiguration.CreateLogger();
+
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.AddAppSettingsSecretsJson()
+                .UseAutofac()
+                .UseSerilog();
+            if (IsMigrateDatabase(args))
+            {
+                builder.Services.AddDataMigrationEnvironment();
+            }
+            await builder.AddApplicationAsync<abp_todolistModule>();
+            var app = builder.Build();
+            await app.InitializeApplicationAsync();
+
+            if (IsMigrateDatabase(args))
+            {
+                await app.Services.GetRequiredService<abp_todolistDbMigrationService>().MigrateAsync();
+                return 0;
+            }
+
+            Log.Information("Starting abp_todolist.");
+            await app.RunAsync();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            if (ex is HostAbortedException)
+            {
+                throw;
+            }
+
+            Log.Fatal(ex, "abp_todolist terminated unexpectedly!");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    private static bool IsMigrateDatabase(string[] args)
+    {
+        return args.Any(x => x.Contains("--migrate-database", StringComparison.OrdinalIgnoreCase));
+    }
+}
