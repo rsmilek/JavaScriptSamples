b0118677a7a20d3b866b9bb49f5f4bf3ea573a1f
Radim Smilek <smilek.radim@gmail.com>
2024-02-24 17:14:41 +0100

    + added ToDoItem handing

---
+++ abp_todolist/aspnet-core/abp_todolist/Entities/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace abp_todolist.Entities
+{
+    public class TodoItem : BasicAggregateRoot<Guid>
+    {
+        public string Text { get; set; }
+    }
+}
