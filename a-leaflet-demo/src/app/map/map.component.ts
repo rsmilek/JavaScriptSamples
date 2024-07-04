907f22ad2528e5ef8141ec1a370a925043efe00d
rsmilek <rsmilek@seznam.cz>
2024-07-04 08:03:57 +0200

    a-leaflet-demo: added marker, circle, polygon

--- a-leaflet-demo/src/app/map/map.component.ts
+++ a-leaflet-demo/src/app/map/map.component.ts
@@ -15,14 +15,22 @@ export class MapComponent implements AfterViewInit {
     this.initMap();
   }
 
-/** Add into section style in angular.json:
+/** 
+ * Modify angular.json:
+ * 
+ * 1. Add into 'style' section
  * "./node_modules/leaflet/dist/leaflet.css"
+ * 
+ * 2. Add into 'assets' section
+ * "./node_modules/leaflet/dist/images"
 */
 
   private initMap(): void {
+
+    // var map = L.map('map').setView([51.505, -0.09], 13);
     this.map = Leaflet.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [ 51.505, -0.09 ],
+      zoom: 13
     });
 
     const tiles = Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -30,8 +38,23 @@ export class MapComponent implements AfterViewInit {
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
-
     tiles.addTo(this.map);
+
+
+    const marker = Leaflet.marker([51.5, -0.09]).addTo(this.map);
+
+    const circle = Leaflet.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(this.map);
+
+    const polygon = Leaflet.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+    ]).addTo(this.map);
   }
 
 }
