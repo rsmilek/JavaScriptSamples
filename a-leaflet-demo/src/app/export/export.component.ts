0bd2918936fa706b05dab79c7d8e0f284e96832f
rsmilek <rsmilek@seznam.cz>
2024-07-08 08:10:33 +0200

    leaflet: added image data export

--- a-leaflet-demo/src/app/export/export.component.ts
+++ a-leaflet-demo/src/app/export/export.component.ts
@@ -4,14 +4,24 @@ import './leaflet-easyexport';
 
 Leaflet.Icon.Default.imagePath = 'assets/';
 
+function PokusFnc(value: string): any {
+  console.log(`PokusFnc ${value}`);
+}
+
 @Component({
   selector: 'app-export',
   templateUrl: './export.component.html',
   styleUrl: './export.component.scss'
 })
 export class ExportComponent implements AfterViewInit {
+
+  // Default image size - ATTENTION: easyPrint changes width -> height for landscape / portrait
+  private readonly defaultWidth: number = 500;
+  private readonly defaultHeight: number = 375;
+
   private map!: Leaflet.Map;
   private printer!: any;
+  private imgData!: string;
 
   constructor() { }
 
@@ -19,10 +29,18 @@ export class ExportComponent implements AfterViewInit {
     this.initMap();
   }
 
+  getImgData() {
+    return this.imgData;
+  }
+
   public onPrintManual() {
-    this.printer.printMap('CurrentSize', 'MyManualPrint')
-    // this.printer.printMap('A4Landscape page', 'MyManualPrint')
-    // this.printer.printMap('A4Portrait page', 'MyManualPrint')
+    // this.printer.printMap('CurrentSize', 'MyManualPrint');
+    // this.printer.printMap('A4Landscape page', 'MyManualPrint');
+
+    this.printer.options.a4PageSize.width = 400;
+    this.printer.options.a4PageSize.height = 400;
+    this.printer.options.exportSaveDialog = false;
+    this.printer.printMap('A4Landscape page', 'MyManualPrint');
   }
 
 
@@ -36,8 +54,7 @@ export class ExportComponent implements AfterViewInit {
       maxZoom: 18,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
+    }).addTo(this.map);
 
     const marker = Leaflet.marker([51.5, -0.09]).addTo(this.map);
 
@@ -59,7 +76,19 @@ export class ExportComponent implements AfterViewInit {
       sizeModes: ['Current', 'A4Landscape', 'A4Portrait'],
       filename: 'myMap',
       exportOnly: true,
-      hideControlContainer: true
+      hideControlContainer: true,
+      exportSaveDialog: true,
+      exportDataCallback: (dataUrl: string) => { 
+        this.imgData = dataUrl;
+        // Restore default values after print for further use
+        this.printer.options.a4PageSize.width = this.defaultWidth;
+        this.printer.options.a4PageSize.height = this.defaultHeight;
+        this.printer.options.exportSaveDialog = true;
+      },
+      a4PageSize: {
+        width: this.defaultWidth,
+        height: this.defaultHeight
+    }
     }).addTo(this.map);
 
   }
